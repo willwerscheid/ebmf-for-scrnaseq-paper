bdf3da2d8b24f31810a56e4484f61bd3d4220392
Jason Willwerscheid <willwerscheid@uchicago.edu>
2022-04-22 08:59:32 -0400

    up K and maxiter

--- code/pulseseq_ebmf_fits.R
+++ code/pulseseq_ebmf_fits.R
@@ -5,5 +5,5 @@ source("./code/do_ebmf_fit.R")
 
 pulseseq <- readRDS("./data/pulseseq_pp.rds")
 
-do_ebmf_fit(pulseseq, 2, FALSE, "montoro/snmf.rds", maxiter = 5)
-do_ebmf_fit(pulseseq, 2, TRUE, "montoro/nmf.rds", maxiter = 5)
+do_ebmf_fit(pulseseq, 25, FALSE, "montoro/snmf.rds", maxiter = 250)
+do_ebmf_fit(pulseseq, 25, TRUE, "montoro/nmf.rds", maxiter = 250)
