a433a422c9ba3576828d75ea9bf67a1af8d07928
Jason Willwerscheid <willwerscheid@uchicago.edu>
2022-04-22 08:29:13 -0400

    do one fit per fn call

--- code/pulseseq_ebmf_fits.R
+++ code/pulseseq_ebmf_fits.R
@@ -1,5 +1,9 @@
+library(Matrix)
+library(flashier)
+
 source("./code/do_ebmf_fits.R")
 
 pulseseq <- readRDS("./data/pulseseq_pp.rds")
 
-do_ebmf_fits(pulseseq, 25, "montoro", maxiter = 200)
+do_ebmf_fit(pulseseq, 2, FALSE, "montoro/snmf.rds", maxiter = 5)
+do_ebmf_fit(pulseseq, 2, TRUE, "montoro/nmf.rds", maxiter = 5)
