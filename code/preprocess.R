ddf29988856ae13e7c30119aefc58c36e3e5c89e
Jason Willwerscheid <willwerscheid@uchicago.edu>
2022-04-23 15:52:13 -0400

    update preprocess

--- code/preprocess.R
+++ code/preprocess.R
@@ -1,22 +1,20 @@
-preprocess <- function(dat, genelist = NULL, min.nzcts = 10) {
+preprocess <- function(dat, min.nzcts = 10) {
   size.factors <- Matrix::colSums(dat)
   size.factors <- size.factors / mean(size.factors)
   lunpc <- max(1 / min(size.factors) - 1 / max(size.factors), 1)
-  gene_cts <- Matrix::rowSums(dat > 0)
 
-  if (!is.null(genelist)) {
-    rownames(dat) <- genelist
-  }
-  dat <- dat[gene_cts >= min.nzcts, ]
-
+  # Use liger for most variable gene selection.
   liger.dat <- rliger::createLiger(list(dat = dat))
   liger.dat <- rliger::normalize(liger.dat)
   liger.dat <- rliger::selectGenes(liger.dat)
+
+  gene_cts <- Matrix::rowSums(dat > 0)
+  dat <- dat[gene_cts >= min.nzcts, ]
 
   return(list(
     counts = dat,
     sf = size.factors,
     pc = lunpc,
-    var.genes = liger.dat@var.genes
+    var.genes = intersect(liger.dat@var.genes, rownames(dat))
   ))
 }
